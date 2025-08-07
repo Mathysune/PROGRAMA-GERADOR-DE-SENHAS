8f476c49c93c235fee433147cd17de6184e9e98a
Mathysune <159062706+Mathysune@users.noreply.github.com>
2025-08-07 13:54:00 -0300

    Add files via upload

--- PROGRAMA-GERADOR-DE-SENHAS-main/main.js
+++ PROGRAMA-GERADOR-DE-SENHAS-main/main.js
@@ -1,3 +1,5 @@
+
+
 const checkbox = document.querySelectorAll('.checkbox');
 const campoSenha = document.getElementById('campoSenha');
 const tamanhoSenhaInput = document.getElementById('tamanhoSenha');
@@ -21,4 +23,40 @@ let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
 senha += alfabeto[numeroAleatorio];
 }
 campoSenha.value = senha;
+}
+function classificaSenha(tamanhoSenha) {
+  forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+  if (tamanhoSenha > 11) {
+    forcaSenha.classList.add('forte');
+  } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    forcaSenha.classList.add('media');
+  } else if (tamanhoSenha <= 5) {
+    forcaSenha.classList.add('fraca');
+  }
+}
+const forcaSenha = document.querySelector('.forca');
+function geraSenha() {
+if (tamanhoSenha > 11) {
+    forcaSenha.classList.add('forte');
+  } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    forcaSenha.classList.add('media');
+  } else if (tamanhoSenha <= 5) {
+    forcaSenha.classList.add('fraca');
+  }
+}
+{
+  let caracteres = '';
+  if (checkbox[0].checked) caracteres += letrasMaiusculas;
+  if (checkbox[1].checked) caracteres += letrasMinusculas;
+  if (checkbox[2].checked) caracteres += numeros;
+  if (checkbox[3].checked) caracteres += simbolos;
+  const tamanhoSenha = parseInt(tamanhoSenhaInput.value);
+  let senha = '';
+  for (let i = 0; i < tamanhoSenha; i++) {
+    const index = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres.charAt(index);
+  }
+  campoSenha.value = senha;
+  classificaSenha(tamanhoSenha); // chama a função de classificação
 }
